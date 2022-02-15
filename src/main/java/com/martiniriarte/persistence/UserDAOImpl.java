7a2c8e098ad1ef03b7a8962a7418637178c215a2
Martin <iriartemartin2@gmail.com>
2022-02-15 08:55:49 -0300

    Se implementa la eliminacion de usuarios.

--- src/main/java/com/martiniriarte/persistence/UserDAOImpl.java
+++ src/main/java/com/martiniriarte/persistence/UserDAOImpl.java
@@ -1,16 +1,35 @@
 package com.martiniriarte.persistence;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.martiniriarte.models.User;
+
 @Repository
 @Transactional
 public class UserDAOImpl {
-	
+
 	@PersistenceContext
-	private EntityManager entityManager;	
+	private EntityManager entityManager;
+
+	public List<User> getUsers() {
+		String query = "SELECT u FROM User";
+		List<User> users = new ArrayList<User>();
+		for (Object user : entityManager.createQuery(query).getResultList()) {
+			User userCast = (User) user;
+			users.add(userCast);
+		}
+		return users; 	
+	}
 	
+	public void delete(Long id) {
+		User user = entityManager.find(User.class, id);
+		entityManager.remove(user);
+	}
 }
--- src/main/resources/static/js/usuarios.js
+++ src/main/resources/static/js/usuarios.js
@@ -7,6 +7,8 @@ $(document).ready(function() {
 
 });
 
+
+// carga a los usuarios en la tabla de usuarios.html
 async function cargarUsuarios() {
 	const request = await fetch('usuarios', {
 		method: 'GET',
@@ -18,17 +20,35 @@ async function cargarUsuarios() {
 	});
 	const usuarios = await request.json();
 
-	console.log(usuarios);
+	
 
 	let listadoHtml = '';
-
 	for (let usuario of usuarios) {
-		let usuarioHtml = '<tr><td>'+ usuario.id +'</td><td>' + usuario.name + ' ' + usuario.lastName + '</td><td>'
-					+ usuario.email + '</td><td>'
-					+ usuario.telephone + '</td><td><a href="#" class="btn btn-danger btn-circle btn-sm"><i class="fas fa-trash"></i></a></td></tr>'
+		let botonEliminar = '<a href="#" onclick="eliminarUsuario(' + usuario.id + ')" class="btn btn-danger btn-circle btn-sm"><i class="fas fa-trash"></i></a>';
+		
+		let usuarioHtml = '<tr><td>' + usuario.id + '</td><td>' + usuario.name + ' ' + usuario.lastName + '</td><td>'
+			+ usuario.email + '</td><td>'
+			+ usuario.telephone + '</td><td>' + botonEliminar + '</td></tr>'
 		listadoHtml += usuarioHtml;
 	}
 
 	document.querySelector('#usuarios tbody').outerHTML = listadoHtml
+}
 
+async function eliminarUsuario(id){
+	
+	if(!confirm('¿Desea eliminar este usuario?')){
+		return;
+	}
+	
+	await fetch('usuarios/' + id, {
+		method: 'DELETE',
+		headers: {
+			'Accept': 'application/json',
+			'Content-Type': 'application/json'
+		},
+
+	});
+	
+	location.reload();
 }
